39535853e519dbaf8be7d294310093d8c31bdee5
joalvarez <alvarez.joaquinez@gmail.com>
2024-10-16 20:35:40 -0300

    chore(editorconfig): add editorconfig file

--- repository/product_repository.go
+++ repository/product_repository.go
@@ -34,7 +34,7 @@ func GetProductById(id string) (model.Product, error) {
 	var product model.Product
 	err := database.DB.QueryRow("SELECT id, name, description, price FROM products WHERE id = $1", id).Scan(
 		&product.Id, &product.Name, &product.Description, &product.Price)
-	
+
 	if err != nil {
 		if err == sql.ErrNoRows {
 			return product, nil
@@ -47,14 +47,14 @@ func GetProductById(id string) (model.Product, error) {
 
 func CreateProduct(product model.Product) error {
 	_, err := database.DB.Exec("INSERT INTO products (id, name, description, price) VALUES ($1, $2, $3, $4)",
-				product.Id, product.Name, product.Description, product.Price)	
+				product.Id, product.Name, product.Description, product.Price)
 
 	return err
 }
 
 func UpdateProduct(product model.Product) error {
 	_, err := database.DB.Exec("UPDATE products SET name = %2, description = $3, price = $4 WHERE id = $1",
-				product.Id, product.Name, product.Description, product.Price)	
+				product.Id, product.Name, product.Description, product.Price)
 
 	return err
 }
