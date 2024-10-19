ae526e1a42db2708e4b9d0840066abc588519ddb
joalvarez <alvarez.joaquinez@gmail.com>
2024-10-19 08:17:22 -0300

    chore: add entry point of application

---
+++ cmd/go-gpt-api/main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"context"
+	"log"
+	"log/slog"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/joalvarezdev/go-gpt/internal/config"
+	"github.com/joalvarezdev/go-gpt/internal/http/handler/product"
+	"github.com/joalvarezdev/go-gpt/storage/postgresql"
+)
+
+func main() {
+  // load config
+  cfg := config.MustLoad()
+
+  // database setup
+  storage, err := postgresql.New(cfg)
+  if err != nil {
+    log.Fatal(err)
+  }
+
+  slog.Info("storage initialized")
+
+  // setup router
+  router := http.NewServeMux()
+
+  router.HandleFunc("POST /products", product.CreateProduct(storage))
+
+  // setup server
+  server := http.Server{
+    Addr: cfg.Port,
+    Handler: router,
+  }
+
+  slog.Info("Server started in", slog.String("address", server.Addr))
+
+  done := make(chan os.Signal, 1)
+
+  signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+  go func() {
+
+    err := server.ListenAndServe()
+
+    if err != nil {
+      log.Fatal("failed to start server")
+    }
+  } ()
+
+  <-done
+
+  slog.Info("shutting down the server")
+
+  ctx, cancel := context.WithTimeout(context.Background(), 5 * time.Second)
+
+  defer cancel()
+
+  if err := server.Shutdown(ctx); err != nil {
+    slog.Error("Failed to shutdown server", slog.String("error", err.Error()))
+  }
+
+  slog.Info("server shutdown successfully")
+}
